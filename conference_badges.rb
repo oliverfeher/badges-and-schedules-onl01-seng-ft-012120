f826d7dc8a035b664ba5fe97c0a114c7f08d1ae9
Oliver Feher <olcsee@gmail.com>
2020-01-23 02:40:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def batch_badge_creator(array)
+    array.each {|i| return "Hello, my name is #{i}"}
+end
+
+def badge_maker(name)
+    return "Hello, my name is #{name}"
+end
+
+def assign_method(array)
+    array.each_with_index {|item, index| print "#{index}:#{item}"}
+end
